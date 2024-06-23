5f64e9ada9d9849b4edb979006c7c04130c575a1
Enzo Costa <enzo_costa7@hotmail.com>
2024-06-23 12:32:43 -0300

    feat: create database

---
+++ src/database/db.ts
@@ -0,0 +1,8 @@
+// db.js
+require('dotenv').config()
+const postgres = require("postgres")
+
+const connectionString = process.env.DATABASE_URL
+const sql = postgres(connectionString)
+
+export default sql
